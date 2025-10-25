0128f81b3d487465d24a6f5d7828fe2e229f01bb
Vladimir Maximov <vladimirmaximov2004@mail.ru>
2025-10-25 04:56:01 +0300

    changes for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
+    base: '/t1-hackathon/',
     plugins: [react()],
     resolve: {
         alias: {
